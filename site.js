541dd678c5d61c02cc11fba54f85a652badf63a7
adambc2 <adamb@c2experience.com>
2025-02-02 16:47:40 -0500

    Update site.js

--- site.js
+++ site.js
@@ -1,4 +1,29 @@
-alert("gitedit");
 $(document).ready(function(){
-	alert("jquery");
+	console.log("case-studies-slider")
+	$('.case-studies-slider').multislider({
+	  // endless scrolling
+	  continuous: true,
+
+	  // slide all visible slides, or just one at a time
+	  slideAll: false,  
+  
+	  // autoplay interval
+	  // 0 or 'false' prevents auto-sliding
+	  interval: 2000, 
+
+	  // duration of slide animation
+	  duration: 500,      
+
+	  // pause carousel on hover
+	  hoverPause: true, 
+  
+	  // pause above specified screen width
+	  pauseAbove: null,   
+  
+	  // pause below specified screen width
+	  pauseBelow: null   
+  
+	})
+
+
 });
