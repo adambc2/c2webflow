0cfc4e1ba2021dea6c906aafff21887d9b64fb12
adambc2 <adamb@c2experience.com>
2025-02-13 23:35:49 -0500

    things

--- site.js
+++ site.js
@@ -18,4 +18,41 @@ $(document).ready(function(){
         caseStudiesSlider.data("prev")(); // Move to the previous slide
     });
 
+    setInterval(cycleFaqItems, 3000);
+
+    $('.faq_wrapper').on('mouseenter click', function () {
+        $(this).addClass('interacted');
+    });
+
+    $('.faq_wrapper').on('mouseleave', function () {
+        var $wrapper = $(this);
+        setTimeout(function () {
+            $wrapper.removeClass('interacted');
+        }, 5000); // Resumes cycling after 5 seconds
+    });
 });
+function cycleFaqItems() {
+    $('.faq_wrapper').each(function () {
+        var $wrapper = $(this);
+
+        // Skip cycling if the wrapper has the "interacted" class
+        if ($wrapper.hasClass('interacted')) {
+            return;
+        }
+
+        var $items = $wrapper.find('.faq_item');
+        var $activeItem = $items.filter('.active');
+
+        if ($activeItem.length === 0) {
+            // If no active item, add active class to the first one
+            $items.first().addClass('active');
+        } else {
+            // Otherwise, move to the next item, or loop back to the first
+            var $nextItem = $activeItem.next('.faq_item');
+            if ($nextItem.length === 0) {
+                $nextItem = $items.first();
+            }
+            $activeItem.removeClass('active');
+            $nextItem.addClass('active');
+        }
+    });
